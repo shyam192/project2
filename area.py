04628c2d1425209ca6322e290f14849fc576241c
shyam192 <92352440+shyam192@users.noreply.github.com>
2021-10-12 10:32:09 +0530

    Create area.py

---
+++ area.py
@@ -0,0 +1,16 @@
+#1st code
+from math import pi
+radius=float(input("enter value of radius:"))
+area=pi*radius*radius
+print("area:",area)
+
+
+
+
+
+#2nd code
+#printing the extension of taken file from user
+filename=input("input the filename :")
+f_extension=filename.split(".")
+print("the extension of file is : " +repr(f_extension[-1]))
+
