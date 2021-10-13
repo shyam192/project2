6d94cdc8d30a5f0249b1c50d1525e657817d2e2f
shyam192 <92352440+shyam192@users.noreply.github.com>
2021-10-13 14:51:01 +0530

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,20 @@
+#printing positive number in a list
+
+List1=[12,-7,5,64,-14]  #input list
+
+#printing positive numbers:
+
+print("POSITIVE NUMBERS IN LIST 1 : ")
+for number in List1:
+    if number>0:            #COMPARING NUMBERS
+        print(number)
+
+#list 2
+List2=[12,14,-95,3]     #input list
+
+#printing positive numbers:
+
+print("POSITIVE NUMBERS IN LIST 2 : ")
+for number in List2:
+    if number>0:
+        print(number)
