1c4860d14282ea620b95084b91bb41f38c2f7cc0
shyam192 <92352440+shyam192@users.noreply.github.com>
2021-10-18 00:12:21 +0530

    Create ass1.py
    assignment1

---
+++ ass1.py
@@ -0,0 +1,28 @@
+#assignment1.1
+#QNO 2:
+#calculator
+print("press")
+print("1.additon")
+print("2.subtraction")
+choice=int(input("ENTer your choice:"))
+if choice==1:
+    a=int(input("enter a:"))
+    b=int(input("enter b:"))
+    c=a+b
+    print(c)
+elif choice==2:
+    a=int(input("enter a:"))
+    b=int(input("enter b:"))
+    c=a-b
+    print(c)
+#OUTPUT
+press
+1.additon
+2.subtraction
+ENTer your choice:1
+enter a:4
+enter b:1
+5
+
+
+
