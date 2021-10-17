d9eef66b00684974ad91548574ca18662dd91a24
shyam192 <92352440+shyam192@users.noreply.github.com>
2021-10-17 23:30:48 +0530

    Create assignment.py
    assignment0

---
+++ assignment.py
@@ -0,0 +1,27 @@
+#assignment 0
+#QNO.1
+1)Start
+2)A=last oddd num in  series
+3)y=A+2
+4)B=last even num in series
+5)x=B-2
+6)print x&y
+7)stop
+
+
+#QNO 2
+#by defult in python print statement ends with new line so we use end in print functiion to append any string at the end of output
+#ex:
+print("welcome to")
+print("my world")
+***output will be like this :welcome to
+                             my world***
+print("welcome to",end='-')
+print("my world")
+          #output will be like this :welcome to - my world
+  
+  
+  
+  
+ 
+  
